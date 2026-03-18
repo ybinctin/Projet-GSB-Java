39181b2f808bba953e86bafe6c7eb535a34976c7
autret-b <autret-b@303-acr-m4630g.tec19.saint-louis29.local>
2026-03-18 14:50:42 +0100

    Développement de du DAO Utilisateur.

--- src/program/program.java
+++ src/program/program.java
@@ -1,9 +1,12 @@
 package program;
 import BDD.Connexion;
+import DAO.UtilisateurDAO;
 
 public class program {
 
 	public static void main(String[] args) {
 		Connexion.getInstance();
+		UtilisateurDAO test = new UtilisateurDAO();
+		System.out.println(test.identification("lvillachane", "motdepasse"));
 	}
 }
